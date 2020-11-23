0888299257447b0c01a0cb569e68bdb4fc67a39c
Aija <aija.valleala@gmail.com>
2020-11-23 15:07:15 +0200

    FEAT: create projects UI view

---
+++ src/common/App.tsx
@@ -0,0 +1,182 @@
+import React from 'react'
+import clsx from 'clsx'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+import {
+  AppBar,
+  Container,
+  Divider,
+  Drawer,
+  IconButton,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from '@material-ui/core'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import DashboardIcon from '@material-ui/icons/Dashboard'
+import MenuIcon from '@material-ui/icons/Menu'
+
+import ProjectsView from '../project/Projects'
+import Dashboard from '../dashboard/Dashboard'
+
+const drawerWidth = 240
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+      flexGrow: 1,
+      height: '100vh',
+      overflow: 'auto',
+    },
+    container: {
+      paddingTop: theme.spacing(4),
+      paddingBottom: theme.spacing(4),
+    },
+    drawerPaper: {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    drawerPaperClose: {
+      overflowX: 'hidden',
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up('sm')]: {
+        width: theme.spacing(9),
+      },
+    },
+    fixedHeight: {
+      height: 240,
+    },
+    menuButton: {
+      marginRight: 36,
+    },
+    menuButtonHidden: {
+      display: 'none',
+    },
+    paper: {
+      display: 'flex',
+      flexDirection: 'column',
+      overflow: 'auto',
+      padding: theme.spacing(2),
+    },
+    root: {
+      display: 'flex',
+    },
+    toolbar: {
+      paddingRight: 24, // keep right padding when drawer closed
+    },
+    title: {
+      flexGrow: 1,
+    },
+    toolbarIcon: {
+      alignItems: 'center',
+      display: 'flex',
+      justifyContent: 'flex-end',
+      padding: '0 8px',
+      ...theme.mixins.toolbar,
+    },
+  })
+)
+
+const App: React.FC = () => {
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(true)
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Timesheets
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Router>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+          }}
+          open={open}
+        >
+          <div className={classes.toolbarIcon}>
+            <IconButton onClick={handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
+          <Divider />
+          <div>
+            <ListItem button component={Link} to="/">
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItem>
+            <ListItem button component={Link} to="/projects">
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="Projects" />
+            </ListItem>
+          </div>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.appBarSpacer} />
+          <Container maxWidth="lg" className={classes.container}>
+            <Switch>
+              <Route path="/projects">
+                <ProjectsView />
+              </Route>
+              <Route path="/">
+                <Dashboard />
+              </Route>
+            </Switch>
+          </Container>
+        </main>
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ src/project/ProjectInfo.tsx
@@ -0,0 +1,120 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+  Avatar,
+  Box,
+  Chip,
+  Collapse,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@material-ui/core'
+import DeleteIcon from '@material-ui/icons/Delete'
+import EditIcon from '@material-ui/icons/Edit'
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
+
+import { Project } from '../common/types'
+
+const useStyles = makeStyles({
+  root: {
+    '& > *': {
+      borderBottom: 'unset',
+    },
+  },
+  padding: {
+    paddingBottom: 0,
+    paddingTop: 0,
+  },
+})
+
+const Tags: React.FC<{ tags: string[] | undefined }> = ({ tags }) => {
+  if (tags) {
+    return (
+      <TableCell align="center">
+        {tags.map((tag: string) => (
+          <Chip size="small" key={tag} label={tag} />
+        ))}
+      </TableCell>
+    )
+  }
+  return <TableCell />
+}
+
+const ProjectInfo: React.FC<{ project: Project }> = ({ project }) => {
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
+
+  const created = new Date(project.creationTimestamp).toString()
+  const edited = new Date(project.lastEdited).toString()
+
+  return (
+    <>
+      <TableRow className={classes.root}>
+        <TableCell>
+          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {project.name}
+        </TableCell>
+        <TableCell align="right">{project.client}</TableCell>
+        <TableCell align="center">
+          <Avatar>
+            {project.owner.firstName.charAt(0)}
+            {project.owner.lastName.charAt(0)}
+          </Avatar>
+        </TableCell>
+        <Tags tags={project.tags} />
+        <TableCell align="right">
+          <EditIcon />
+          <DeleteIcon />
+        </TableCell>
+      </TableRow>
+      {/* Collapsible part */}
+      <TableRow>
+        <TableCell className={classes.padding} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box margin={1}>
+              <Typography variant="h6" gutterBottom component="div">
+                Project Details
+              </Typography>
+              <p>Description: {project.description}</p>
+              <p>Employees:</p>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell />
+                    <TableCell>Time</TableCell>
+                    <TableCell>By</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>Created</TableCell>
+                    <TableCell>{created}</TableCell>
+                    <TableCell>
+                      {project.creator.firstName} {project.creator.lastName}
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Last modified</TableCell>
+                    <TableCell>{edited}</TableCell>
+                    <TableCell>{project.lastEditor}</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </>
+  )
+}
+
+export default ProjectInfo
